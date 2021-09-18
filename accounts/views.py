d2bf474ee66e3557e7e60053774294eafcdacbe3
Kursant SDA <aziemba94@gmail.com>
2021-09-18 10:10:14 +0200

    All in

---
+++ accounts/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+from django.views.generic import CreateView
+
+from accounts.forms import (
+    SignUpForm
+)
+
+# Create your views here.
+
+from django.contrib.auth.views import LoginView, PasswordChangeView # <== NOWE
+from django.urls import reverse_lazy
+
+class SubmittableLoginView(LoginView):
+    template_name = 'form.html'
+    success_url = reverse_lazy('index')
+
+
+class SubmittablePasswordChangeForm(PasswordChangeView):
+    template_name = 'form.html'
+    success_url = reverse_lazy('index')
+
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = 'form.html'
+    success_url = reverse_lazy('index')
---
+++ viewer/views.py
@@ -0,0 +1,71 @@
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from django.shortcuts import render
+import datetime
+
+from viewer.models import Movie
+from viewer.forms import MovieForm
+
+from logging import getLogger
+LOGGER = getLogger()
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
+
+@login_required
+def generate_demo(request):
+    our_get = request.GET.get('name', '')
+    return render(
+        request, template_name='demo.html',
+        context={'our_get': our_get,
+                 'list': ['pierwszy', 'drugi', 'trzeci', 'czwarty'],
+                 'nasza_data': datetime.datetime.now()
+                 }
+    )
+
+
+class MoviesView(ListView):
+    template_name = 'movies.html'
+    model = Movie
+
+
+class MovieCreateView(PermissionRequiredMixin, CreateView):
+    template_name = 'formAddEditMovie.html'
+    form_class = MovieForm
+    # adres pobrany z URLs na który zostaniemy przekierowani
+    # gdy walidacja się powiedzie (movie_create pochodzi z name!)
+    success_url = reverse_lazy('movie_create')
+    permission_required = 'viewer.add_movie'
+
+    # co ma się dziać, gdy formularz nie przejdzie walidacji:
+    def form_invalid(self, form):
+        # odkładamy w logach informacje o operacji
+        LOGGER.warning('User provided invalid data')
+        # zwracamy wynik działania pierwotnej funkcji form_invalid
+        return super().form_invalid(form)
+
+class MovieUpdateView(PermissionRequiredMixin, UpdateView):
+    template_name = 'formAddEditMovie.html'
+    form_class = MovieForm
+    # adres pobrany z URLs na który zostaniemy przekierowani
+    # gdy aktualizacja się powiedzie (index pochodzi z name!)
+    success_url = reverse_lazy('index')
+    # Nazwa encji, z której będziemy aktualizować rekord
+    model = Movie
+    permission_required = 'viewer.change_movie'
+
+    # co ma się dziać, gdy formularz nie przejdzie walidacji:
+    def form_invalid(self, form):
+        # odkładamy w logach informacje o operacji
+        LOGGER.warning('User provided invalid data when updating')
+        # zwracamy wynik działania pierwotnej funkcji form_invalid
+        return super().form_invalid(form)
+
+class MovieDeleteView(PermissionRequiredMixin, DeleteView):
+    #Nazwa szablonu wraz z rozszerzeniem którą pobieramy z folderu templates
+    template_name = 'delete_movie.html'
+    success_url = reverse_lazy('index')
+    #Nazwa encji, z której będziemy kasować rekord
+    model = Movie
+    permission_required = 'viewer.delete_movie'
