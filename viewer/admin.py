e447ba4f47847e975c46c21d4bc2660ee80dbd09
Kursant SDA <aziemba94@gmail.com>
2021-09-18 12:31:25 +0200

    Finished Project

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
-
+from django.contrib.admin import ModelAdmin
 # Register your models here.
+
+class MovieAdmin(ModelAdmin):
+
+    @staticmethod
+    def released_year(obj):
+        return obj.released_year
+
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        queryset.update(description=None)
+
+    ordering = ['id']
+    list_display = ['id', 'title', 'genre', 'released_year']
+    list_display_links = ['id', 'title']
+    list_per_page = 20
+    list_filter = ['genre']
+    search_fields = ['title']
+    actions = ['cleanup_description']
+    fieldsets = [
+        (None,{'fields': ['title','created']}),
+        ('External information', {
+            'fields': ['genre', 'released'],
+            'description': 'This fields are external information!'
+        }),
+        ('User information', {
+            'fields': ['rating', 'description'],
+            'description': "This fields will be filled with user"
+        })
+
+
+    ]
+    readonly_fields = ['created']
